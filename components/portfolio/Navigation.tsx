ccb76bb68f244f5bd3fefad42cc95e964466c365
esraahusamia <30745980+esraahusamia@users.noreply.github.com>
2025-10-30 21:26:47 +0300

    Update Navigation.tsx

--- components/portfolio/Navigation.tsx
+++ components/portfolio/Navigation.tsx
@@ -32,7 +32,7 @@ export default function Navigation() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 md:h-20">
           <a href="#" className="font-mono font-bold text-lg md:text-xl text-neutral-900">
-            EA
+            ESRAA HUSAMIA
           </a>
 
           {/* Desktop Navigation */}
